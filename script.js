648a6f2193e7c1c552700accb27f79a8a067a3a2
Thomas Deruere <t.deruere@gmail.com>
2023-02-20 23:19:14 +0100

    ajout generation des boutons left et right et lien avec fonction js testé sur l'ensemble des boutons

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Data
 function httpGet(theUrl)
 {
     var xmlHttp = new XMLHttpRequest();
@@ -20,7 +21,7 @@ const url2 = APIbaseUrl + '?year=1920' + "?sort_by=&title"
 //'?imdb_score=5'
 //"?sort_by= -(pour inverser) dans url avec tous les filtres"
 
-function genMovieList(APIurl, nb = 10) {
+function genMovieList(APIurl, nb = 28) {
     const jsonResult = httpGet(APIurl);
     const APIobj = responseToObject(jsonResult);
     let nextPageUrl = APIobj.next;
@@ -33,6 +34,7 @@ function genMovieList(APIurl, nb = 10) {
         movies.push(movie)
         nb--
         if (nb == 0) {
+            console.log("genmovielist result : " + movies)
             return movies
         }
     }
@@ -49,9 +51,11 @@ function genMovieList(APIurl, nb = 10) {
 
 
 // view
-function genMovieRow(films, rawNumber, rawTitle, nb = 7) {
+function genMovieRow(films, rawNumber, rawTitle, nb = 7, page = 2) {
+    document.querySelector("#movie_row" + String(rawNumber)).innerHTML=""
     // selection du movie_row visé
     const sectionFilms = document.querySelector("#movie_row" + String(rawNumber))
+    
     // Creation du titre sectionTitle
     const headElement = document.createElement("h1")
     headElement.className = "sectionTitle"
@@ -65,6 +69,21 @@ function genMovieRow(films, rawNumber, rawTitle, nb = 7) {
     const imgListElement = document.createElement("ul")
     // Creation de la liste d'image et boucle pour chaque image
     imgListElement.className = "img-list"
+    // Bouton page precedente
+    if (page > 1) {
+        const IdSelectorName = "left" + String(rawNumber)
+        const SectionBouttonLeft = document.createElement("a")
+        SectionBouttonLeft.id = IdSelectorName
+        SectionBouttonLeft.href = "#"
+        const imgElement = document.createElement("img")
+        imgElement.src = "images\\left.png"
+        imgElement.alt = "left_arrow"
+        imgElement.height = 120
+        imgElement.width = 45
+        SectionBouttonLeft.appendChild(imgElement)
+        imgListElement.appendChild(SectionBouttonLeft)
+    }
+    // Liste des films
     for (let i = 0; i < nb; i++) {
         const film = films[i]
         
@@ -74,6 +93,8 @@ function genMovieRow(films, rawNumber, rawTitle, nb = 7) {
         aElement.href = film.imdb_url
         const imgElement = document.createElement("img")
         imgElement.src = film.image_url
+        imgElement.height = 280
+        imgElement.width = 180
         aElement.appendChild(imgElement)
         const spanElement = document.createElement("span")
         spanElement.className = "text-content"
@@ -82,31 +103,72 @@ function genMovieRow(films, rawNumber, rawTitle, nb = 7) {
         liElement.appendChild(aElement)
         imgListElement.appendChild(liElement)
     }
+    // Bouton page suivante
+    if (page < 4) {
+        const IdSelectorNameRight = "right" + String(rawNumber)
+        const SectionBouttonRight = document.createElement("a")
+        SectionBouttonRight.id = IdSelectorNameRight
+        SectionBouttonRight.href = "#"
+        const imgElement = document.createElement("img")
+        imgElement.src = "images\\right.png"
+        imgElement.alt = "right_arrow"
+        imgElement.height = 120
+        imgElement.width = 45
+        SectionBouttonRight.appendChild(imgElement)
+        imgListElement.appendChild(SectionBouttonRight)
+    }
     sectionFilms.appendChild(imgListElement)
 }
 
 // On genere le row des meilleurs films par note imdb
 const url_bestImdbScores = APIbaseUrl + "?sort_by=-imdb_score&imdb_score_min=9"
-const movies2 = genMovieList(url_bestImdbScores, 7)
+const movies2 = genMovieList(url_bestImdbScores)
 genMovieRow(movies2, 2, "Meilleurs scores ImDb")
 
 // On genere le row des meilleurs films par catégorie "Drama"
 const url_bestDramas = APIbaseUrl + "?sort_by=-imdb_score&genre_contains=Animation"
-const movies3 = genMovieList(url_bestDramas, 7)
+const movies3 = genMovieList(url_bestDramas)
 genMovieRow(movies3, 3, "Meilleurs films d'animation")
 
-// On genere le row des meilleurs films de 2022
-const url_best2022 = APIbaseUrl + "?sort_by=-imdb_score&gyear=2022"
-const movies4 = genMovieList(url_best2022, 7)
-genMovieRow(movies4, 4, "Meilleurs films de 2022")
+// On genere le row des meilleurs films de Tarantino
+const url_bestTarantino = APIbaseUrl + "?sort_by=-imdb_score&writer_contains=tarantino&director_contains=tarantino"
+const movies4 = genMovieList(url_bestTarantino)
+genMovieRow(movies4, 4, "Meilleurs films de Quentin Tarantino")
 
 // document.querySelector(".films").innerHTML = ""
 
-const SectionBoutton = document.querySelector("#left1")
+
+const SectionBouttonLeft1 = document.querySelector("#left1")
 const imgElement = document.createElement("img")
 imgElement.src = "images\\left.png"
 imgElement.alt = "left_arrow"
-imgElement.height = 260
+imgElement.height = 120
 imgElement.width = 45
-SectionBoutton.appendChild(imgElement)
+SectionBouttonLeft1.appendChild(imgElement)
+
+const SectionBouttonRight1 = document.querySelector("#right1")
+const imgElement2 = document.createElement("img")
+imgElement2.src = "images\\right.png"
+imgElement2.alt = "right_arrow"
+imgElement2.height = 120
+imgElement2.width = 45
+SectionBouttonRight1.appendChild(imgElement2)
+
+
+for (let i = 0; i < 4; i++) {
+    let leftId = "left" + String(i+1)
+    document.getElementById(leftId).onclick = function() {myFunction(leftId)};
+    console.log("init bouton id : " + leftId)
+    let rightId = "right" + String(i+1)
+    document.getElementById(rightId).onclick = function() {myFunction(rightId)};
+    console.log("init bouton id : " + rightId)
+
+}
+// document.getElementById("left1").onclick = function() {myFunction("left1")};
+  
+// document.getElementById("right1").onclick = function() {genMovieRow(movies2, 1, "coucou")};
+  
+function myFunction(elementId) {
+  console.log("tu a cliqué sur " + elementId);
+}
 console.log("je suis à la fin du script js :)")
