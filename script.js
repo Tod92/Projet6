3d37e098ee714beed38a2f92010a51bbb6b1538d
Thomas Deruere <t.deruere@gmail.com>
2023-02-23 12:36:46 +0100

    affichage modal ok. Pb infos  insuffisantes recuperée de l'api pour inserer dans modal

--- script.js
+++ script.js
@@ -93,7 +93,7 @@ function genMovieRow(films, rawNumber, rawTitle, nb = 7, page = 1) {
         liElement.className = "image"
         const aElement = document.createElement("a")
         aElement.href = "javascript:void(0)"
-        aElement.id = "movie" + String(i)
+        aElement.id = "movie" + String(rawNumber) + String(i)
         const imgElement = document.createElement("img")
         imgElement.src = film.image_url
         imgElement.height = 280
@@ -168,14 +168,47 @@ class Row {
     }
     showModal(movie_index) {
         movie_index += (this.page - 1) * 7;
-        movie = this.movie[movie_index]
-        console.log("MODAL DEMANDE POUR :" +movie.title)
+        if (this.number == 1) {
+            movie_index +=1;
+        }
+        let movie = this.movies[movie_index]
+        console.log("MODAL DEMANDE POUR :" + movie.title)
+        var modal = document.getElementById("myModal");
+        var btn = document.getElementById("myBtn");
+        var span = document.getElementsByClassName("close")[0];
+        var modalText = document.getElementById("modal-text");
+        modalText.innerHTML = ""
+        // modalText.innerHTML =
+        var htmlConstruct = "<h2>" + movie.title + "</h1>" +
+                            "<img src=\"" + movie.image_url + "\">" +
+                            "Genres :"
+        for (const genre of movie.genres) {
+            htmlConstruct += " " + genre
+        }
+        htmlConstruct += "\n" + "Année de sortie : " + movie.year
+        
+        modalText.innerHTML = htmlConstruct
+        
+                            
+        modal.style.display = "block";
+        
+       // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+            modal.style.display = "none";
+          }
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target == modal) {
+            modal.style.display = "none";
+            }
+        }
     }
 }
 
 
 // On genere le row des meilleurs films par note imdb
 // ! specificité : on isole le premier résultat de la liste pour la vignette meilleur film
+
 const url_bestImdbScores = APIbaseUrl + "?sort_by=-imdb_score&imdb_score_min=9"
 const row1 = new Row(1, url_bestImdbScores, "Meilleurs scores ImDb", 1)
 const bestMovie = row1.movies[0]
@@ -215,23 +248,24 @@ function Listener() {
         let rowId = "movie_row" + String(i +1)
         const rowElement = document.querySelector(rowId)
         for (let a = 0; a < 7; a++) {
-            let aId = "movie" + String(a)
+            let aId = "movie" + String(i+1) + String(a)
             console.log("aId = " + aId)
-            rowElement.getElementById(aId).onclick = function() {onImgClick(rowId,a)};
+            document.getElementById(aId).onclick = function() {onImgClick(rowId,a)};
 
         }
+
     }
 }
 
 function onImgClick(row_id, movie_index) {
     console.log("tu a cliqué sur " + row_id + "image " + movie_index)
-    if (rowId == "movie_row1") {
+    if (row_id == "movie_row1") {
         row1.showModal(movie_index)
     }
-    if (rowId == "movie_row2") {
+    if (row_id == "movie_row2") {
         row2.showModal(movie_index)
     }
-    if (rowId == "movie_row3") {
+    if (row_id == "movie_row3") {
         row3.showModal(movie_index)
     }
 }
@@ -283,29 +317,3 @@ function onArrowClick(elementId) {
 
   console.log("je suis à la fin du script js :)")
 
-  // Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.innerHTML = ""
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-} 
